1653c335d0469ed1316723126adc8b3af79a6aec
Alan Mabry <alan.mabry@gmail.com>
2023-08-25 13:30:05 -0400

    reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TopNav from './features/TopNav';
 import SearchBar from './features/SearchBar';
 import Footer from './features/Footer';
 import SearchResults from './features/SearchResults.tsx';
+import './styles/reset.css';
 import './App.css';
 
 function App() {
